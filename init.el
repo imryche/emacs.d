ee0d7e430c50e0f91e070f6ff6e322d8f661b53a
oneor0 <oneor00@gmail.com>
2021-04-08 21:22:29 +0300

    use pyimport instead of custom function

--- init.el
+++ init.el
@@ -440,6 +440,8 @@
     :states '(normal visual emacs)
     "b" 'blacken-buffer))
 
+(use-package pyimport)
+
 (use-package web-mode)
 
 (use-package yaml-mode)
@@ -500,15 +502,6 @@
   (balance-windows)
   (other-window 1))
 
-(defun oneor0/autoflake-buffer()
-  "autoflake --remove-all-unused-imports -i unused_imports.py"
-  (interactive)
-  (if (executable-find "autoflake")
-      (progn
-        (shell-command (format "autoflake --remove-all-unused-imports -i %s"
-                               (shell-quote-argument (buffer-file-name))))
-        (revert-buffer t t t))
-    (message "Error: Cannot find autoflake executable.")))
 
 (defun insert-line-above ()
   "Insert an empty line above the current line."
