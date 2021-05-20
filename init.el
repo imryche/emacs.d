3bcfa4f62fe192f562975c0f957d3b377683f4ff
imryche <oneor00@gmail.com>
2021-05-20 16:48:41 +0300

    remove unused keybinding to projectile keymap

--- init.el
+++ init.el
@@ -366,8 +366,7 @@
 (use-package projectile
   :config
   (projectile-mode +1)
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map))
 
 ;; Writing
 (use-package darkroom
