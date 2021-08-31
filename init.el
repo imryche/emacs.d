f1bfd0314374b0b4ed0571e0d81a2b3831166601
imryche <oneor00@gmail.com>
2021-08-31 09:44:35 +0300

    use package.el

--- init.el
+++ init.el
@@ -5,23 +5,20 @@
 
 ;;; Code:
 
-(setq byte-compile-warnings '(cl-functions))
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(require 'use-package)
+(require 'use-package-ensure)
 
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
+(setq use-package-always-ensure t)
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
