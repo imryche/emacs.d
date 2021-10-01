be72454d3b26f46a263c3c9fe9ba1a9832351623
imryche <imryche13@gmail.com>
2021-10-01 20:59:59 +0300

    refactoring

--- init.el
+++ init.el
@@ -430,10 +430,10 @@
 (use-package org
   :defer t
   :init
-  (setq org-log-done 'time)
-  (setq org-directory "~/Dropbox/org")
-  (setq org-agenda-files (list "~/Dropbox/org/projects/"))
-  (setq org-link-frame-setup '((file . find-file)))
+  (setq org-log-done 'time
+        org-directory "~/Dropbox/org"
+        org-agenda-files (list "~/Dropbox/org/projects/")
+        org-link-frame-setup '((file . find-file)))
   :config
   (add-hook 'org-mode-hook 'visual-line-mode)
   (defun org-file-path (filename)
